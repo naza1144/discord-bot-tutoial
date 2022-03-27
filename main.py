3c4d6d78879f274b9b7dacd94ce7422934b1806d
naza <knnbos77@gmail.com>
2022-03-27 19:06:31 +0700

    bot-2

--- main.py
+++ main.py
@@ -1,16 +1,36 @@
 import discord
 from discord.ext import commands
 
+bot = commands.Bot(command_prefix=commands.when_mentioned_or('!'))
+
 client = commands.Bot(command_prefix = '.')
 
 token = "OTM5NzkxMDE0NzQxMjk5MjEx.Yf9-lg.adrhrX9RpuvMUc7Z1VYL3pdB4yo"
 
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print('Logged on as', self.user)
 
+    async def on_message(self, message):
+        # don't respond to ourselves
+        if message.author == self.user:
+            return
 
+        if message.content == '!Agnaktor':
+           if message.content.startswith('!Agnaktor'):
+            await message.channel.send('Hee!')
 
-@client.event
-async def on_ready():
-    print('Bot is ready')
+           
+  
+        if message.content.startswith('!Agnaktor'):
+        #title
+           embedVar = discord.Embed(title="Agnaktor", color=0xE67E22)
+
+       #object
+           embedVar.set_thumbnail(url='https://static.wikia.nocookie.net/monsterhunter/images/6/64/MHXR-Purgatory_Agnaktor_Icon.png/revision/latest/scale-to-width-down/128?cb=20190328020518')
+           embedVar.set_footer(text='จุดสำคัญ')
+           embedVar.set_image(url='https://static.wikia.nocookie.net/monsterhunter/images/1/1a/DMG-Agnaktor.png/revision/latest/scale-to-width-down/760?cb=20100712064900')
+           await message.channel.send(embed=embedVar)
 
 
 @client.command(pass_context=True)
@@ -72,6 +92,15 @@ async def on_message(message):
         embedVar.set_image(url='https://cdn.discordapp.com/attachments/700558744211423245/957297503630405773/unknown.png')
         await message.channel.send(embed=embedVar)
     
+    elif message.content.startswith('+Arzuros'):
+        #title
+        embedVar = discord.Embed(title="Arzuros", color=0x2879EC)
+
+       #object
+        embedVar.set_footer(text='จุดสำคัญ')
+        embedVar.set_thumbnail(url='https://static.wikia.nocookie.net/monsterhunter/images/5/52/MHP3-Arzuros_Icon.png/revision/latest/scale-to-width-down/62?cb=20160623223638')
+        embedVar.set_image(url='https://cdn.discordapp.com/attachments/700558744211423245/957297503630405773/unknown.png')
+        await message.channel.send(embed=embedVar)
 
 
 
@@ -90,4 +119,5 @@ async def on_message(message):
 
 
 
+client = MyClient()
 client.run(token)
